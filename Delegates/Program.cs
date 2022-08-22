2bd5109c8f94bc636c7f65f603060003d7b3a310
Omer Faruk Sandikci <sandikci_omer@hotmail.com>
2022-08-22 19:27:30 +0300

    Add project files.

---
+++ Delegates/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Delegate worldDelegate = WriteHello;
+            worldDelegate += GoodBye;
+
+            worldDelegate();
+        }
+
+
+        delegate void Delegate();
+
+        static void WriteHello()
+        {
+            Console.WriteLine("Hello World!");
+        
+        }
+
+        static void GoodBye()
+        {
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
---
+++ FuncDelegate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FuncDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FuncExamp(Apply);                                       //Send function parameter
+            FuncExamp((x, y) => x + y);                             //Send inline function parameter
+            FuncExamp(delegate (int x, int y) { return x + y; });   //Send inline delegate parameter
+        }
+
+        static void FuncExamp(Func<int, int, int> funcInstance)
+        {
+            var result = funcInstance(2, 3);
+            Console.WriteLine(result);
+        }
+
+
+        static int Apply(int param1, int param2)
+        {
+            return param1 + param2;
+        }
+
+        static int Extraction(int param1, int param2)
+        {
+            return param1 - param2;
+        }
+    }
+}
